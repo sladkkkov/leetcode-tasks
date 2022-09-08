21091e33f520469bfa6aa6db883c9b4a5d82643c
Danila Sladkov <sladkkov@yandex.ru>
2022-09-09 01:15:54 +0400

    PalindromeNumber9

---
+++ src/main/java/ru/sladkkov/leetcodetasks/LeetCodeTasksApplication.java
@@ -0,0 +1,14 @@
+package ru.sladkkov.leetcodetasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeTasksApplication {
+
+    public static void main(String[] args) {
+        System.out.println(PalindromeNumber9.secondSolution(121));
+
+    }
+
+}
---
+++ src/main/java/ru/sladkkov/leetcodetasks/PalindromeNumber9.java
@@ -0,0 +1,64 @@
+package ru.sladkkov.leetcodetasks;
+
+public class PalindromeNumber9 {
+    /*
+    Runtime: 29 ms, faster than 11.19% of Java online submissions for Palindrome Number.
+    Memory Usage: 47 MB, less than 10.87% of Java online submissions for Palindrome Number.
+    */
+    public static boolean firstSolution(int x) {
+        return x >= 0 && String.valueOf(x).equals(new StringBuilder(String.valueOf(x)).reverse().toString());
+    }
+
+    /*
+    Runtime: 13 ms, faster than 71.76% of Java online submissions for Palindrome Number.
+    Memory Usage: 44.6 MB, less than 53.41% of Java online submissions for Palindrome Number.
+    */
+    public static boolean secondSolution(int x) {
+        if (x < 0) return false;
+
+        int digit = x;
+        int reverse = 0;
+
+        while (digit != 0) {
+            int remainder = digit % 10;
+            reverse = reverse * 10 + remainder;
+            digit = digit / 10;
+        }
+
+        return x == reverse;
+    }
+
+    /*
+    Runtime: 9 ms, faster than 98.03% of Java online submissions for Palindrome Number.
+    Memory Usage: 41.6 MB, less than 95.21% of Java online submissions for Palindrome Number.
+    */
+    public static boolean thirdSolution(int x) {
+        if (x >= 0 && x < 10) return true;
+        if (x < 0) return false;
+
+        int digit = x;
+        int reverse = 0;
+        int remainder;
+
+        while (digit != 0) {
+            remainder = digit % 10;
+            reverse = reverse * 10 + remainder;
+            digit = digit / 10;
+        }
+
+        return x == reverse;
+    }
+
+    public static boolean fouthSolution(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber/10;
+    }
+}
